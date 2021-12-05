5a051f6ea9f82122756ea2adc3be96b517a803e4
jackie1santana <jackietristansantana@gmail.com>
2021-12-05 16:01:29 -0600

    fixed inline sourcemap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+
 import App from './App';
 
-const browserHistory = createBrowserHistory();
+
 
 ReactDOM.render(
   <Router>
