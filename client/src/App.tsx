c14f1227be25cabf3d184e4e69d40043e411f058
jackie1santana <jackietristansantana@gmail.com>
2021-12-05 21:17:09 -0600

    added authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import {Home} from './components/Home'
 import {Login} from './auth/Login'
-import {Register} from './auth/Register
+import {Register} from './auth/Register'
 import {Dashboard} from './components/Dashboard'
-
+const isAdmin = true
   const App = () => (
     <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+      {/* Add context in */}
+     {
+         isAdmin &&  <Route path="/dashboard" element={<Dashboard />} />
+     }
     </Routes>
   </Router>
   );
