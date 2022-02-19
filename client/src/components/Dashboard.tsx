626ab57e67d7e711a0ed5fdeb6a6636a7570abea
jackie1santana <jackietristansantana@gmail.com>
2022-02-18 23:29:47 -0600

    added chrome debugger

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-
+let name = 'jackie';
+debugger;
 // Login
 export const Dashboard = () => (
     <div style={{textAlign: 'center'}}>
+    
         <h1>Dashboard</h1>
         <h2>Welcome User</h2>
       <button>Logout</button>
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,19 @@ import path from 'path';
 const app: Application = express();
 
 const PORT = 5000;
-const publicDirectory = path.join(__dirname, '../../client/build');
-app.use(express.static(publicDirectory));
+// const publicDirectory = path.join(__dirname, '../../client/build');
+// app.use(express.static(publicDirectory));
 
 // app.get('*', (req, res) => {
 //   res.sendFile(publicDirectory);
 // });
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!');
+  debugger;
+  res.send('Hello World!!!!');
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
